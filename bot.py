36ddeb7f2021c7fd685b0115264ce710c5cff388
alexanderaziz <alexanderaziz04@hotmail.com>
2023-10-07 02:32:16 -0400

    changed command prefix modularity

--- bot.py
+++ bot.py
@@ -5,8 +5,6 @@ from bs4 import BeautifulSoup
 import requests
 import constants
 
-from discord import app_commands
-from discord.ext import commands
 
 TOKEN = constants.TOKEN
 intents = discord.Intents.default()
@@ -20,53 +18,39 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'logged in as {client.user}')
     await schedule_daily_message(True)
-
-
-async def global_setup():
-    await new_category_global(constants.CATEGORY_NAME)
-    await new_text_channel_global(constants.ANOUNCEMENT_CHANNEL_NAME, constants.CATEGORY_NAME)
-    await schedule_daily_message(True)
-
 
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
-    guild = message.guild
-    if message.content.startswith('$'):
-        message.content = message.content[1:]
-
-        if message.content == 'hello':
-            await message.channel.send('Hello!')
-
-        elif message.content == 'help':
-            await message.channel.send('List of Commands:\n'
-                                       '$help  -  Shows a list of commands\n'
-                                       '$setup -  Creates the designated channel and category for the bot to work\n'
+    if message.content.startswith(constants.COMMAND_PREFIX):
+        message.content = message.content[len(constants.COMMAND_PREFIX):]
+        if message.content == 'help':
+            await message.channel.send('List of Commands:\n' +
+                                       constants.COMMAND_PREFIX + 'help  -  Shows a list of commands\n' +
+                                       constants.COMMAND_PREFIX + 'setup -  Creates the designated channel and category for the bot to work\n' +
                                        '\n'
-                                       'In case the bot stops working:\n'
+                                       'In case the bot stops working:\n' +
                                        '1. delete all categories associated with bot\n'
                                        '2. make sure no channels/categories conflict with the bots naming scheme, Currently it is: "' + constants.CATEGORY_NAME + '" and ' + '"' + constants.ANOUNCEMENT_CHANNEL_NAME + '"\n'
-                                       '3. use $setup')
+                                                                                                                                                                                                                        '3. use ' + constants.COMMAND_PREFIX + 'setup')
         elif message.content == 'setup':
             if message.author.guild_permissions.administrator:
                 await setup_local(message)
             else:
                 await message.channel.send('You do not have permissions to use this command. You must be an admin')
         else:
-            print(f'${message.content} is not a valid command therefore nothing sent in channel')
+            await message.channel.send(
+                constants.COMMAND_PREFIX + message.content + ' is not a valid command. Use ' + constants.COMMAND_PREFIX + 'help for a list of valid commands')
     else:
         return
 
 
-
 async def setup_local(message):
     guild = message.guild
-    #check if you have required perms in guild here
 
     await message.channel.send('Setting up....')
 
@@ -78,20 +62,19 @@ async def setup_local(message):
     await new_text_channel_local(guild, new_solution_text_channel, constants.CATEGORY_NAME)
 
     link = await daily_leetcode_scraper()
-    await announcement_local(guild, "@everyone\nDaily LeetCode Challenge: " + link + " \nPost your solutions in #put something here!")
+    await announcement_local(guild,
+                             "@everyone\nDaily LeetCode Challenge: " + link + " \nPost your solutions in #put something here!")
 
     await message.channel.send('Done setting up see #' + constants.ANOUNCEMENT_CHANNEL_NAME + ' for the daily leetcode')
     await message.channel.send(
         'Please do not change the naming of the category/channels created by this bot, however you may move them around')
 
 
-
-#calls announcement_global at the end eventually
 async def schedule_daily_message(initial):
     link = await daily_leetcode_scraper()
     t = datetime.datetime.now()
 
-    new_solution_text_channel = str(t.year) + "-" +  str(t.month) + "-"+ str(t.day) + "-solutions"
+    new_solution_text_channel = str(t.year) + "-" + str(t.month) + "-" + str(t.day) + "-solutions"
     text = "@everyone \nDaily LeetCode Challenge: " + link + " \nPost your solutions in #" + new_solution_text_channel
     if (initial):
         await announcement_global(text)
@@ -117,12 +100,12 @@ async def schedule_daily_message(initial):
 async def lock_yesterday_solution(new_solution_text_channel):
     t = datetime.datetime.now()
     yesterday = t.replace(day=t.day - 1)
-    old_channel_name = str(yesterday.year) + "-" +  str(yesterday.month) + "-" + str(yesterday.day) + "-solutions"
+    old_channel_name = str(yesterday.year) + "-" + str(yesterday.month) + "-" + str(yesterday.day) + "-solutions"
     for guild in client.guilds:
         for category in guild.categories:
-            if(category.name == constants.CATEGORY_NAME):
+            if (category.name == constants.CATEGORY_NAME):
                 for channel in category.text_channels:
-                    if(channel.name == old_channel_name):
+                    if (channel.name == old_channel_name):
                         await channel.set_permissions(guild.default_role, send_messages=False, read_messages=True)
                         await channel.edit(name="🔒" + old_channel_name)
                         for new_channel in category.text_channels:
@@ -133,6 +116,7 @@ async def lock_yesterday_solution(new_solution_text_channel):
 async def announcement_global(announcement_string):
     for guild in client.guilds:
         await announcement_local(guild, announcement_string)
+
 
 async def announcement_local(guild, announcement_string):
     for category in guild.categories:
@@ -148,31 +132,34 @@ async def daily_leetcode_scraper():
     links = soup.findAll("a", href=True)
     problem_links = []
     for link in links:
-        if (link['href'].startswith('/problems/')):
+        if link['href'].startswith('/problems/'):
             problem_links.append(link['href'])
 
     now = datetime.datetime.now()
     for x in problem_links:
-        if (x[-10:] == str(now.year) + "-" + str(now.month).zfill(2) + "-" + str(now.day).zfill(2)):
+        if x[-10:] == str(now.year) + "-" + str(now.month).zfill(2) + "-" + str(now.day).zfill(2):
             return "https://leetcode.com" + x
 
     # send error message in channel which is initialized
     return "NULL: ERROR"
 
+
 async def new_text_channel_global(new_channel_name, category_name):
     for guild in client.guilds:
         await new_text_channel_local(guild, new_channel_name, category_name)
 
+
 async def new_text_channel_local(guild, new_channel_name, category_name):
     for category in guild.categories:
-        if (category.name == category_name):
+        if category.name == category_name:
             channel_found = False
             for channel in category.text_channels:
                 if channel.name == new_channel_name:
                     channel_found = True
-            if (not channel_found):
+            if not channel_found:
                 await category.create_text_channel(new_channel_name)
     return
+
 
 async def new_category_global(new_category_name):
     for guild in client.guilds:
@@ -185,6 +172,7 @@ async def new_category_global(new_category_name):
             await guild.create_category(new_category_name)
     return
 
+
 async def new_category_local(guild, new_category_name):
     found_category = False
     for category in guild.categories:
@@ -193,7 +181,6 @@ async def new_category_local(guild, new_category_name):
 
     if not found_category:
         await guild.create_category(new_category_name)
-
 
 
 client.run(TOKEN)
--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
-TOKEN = 'MTE1NDkwNjc1MTk0NzI1NTg1OQ.G30zzv.u9IvdohrXYF5fKl9QnLLRto84xvneFTvuxqIdw'
-CATEGORY_NAME = 'Leetcode Daily'
-ANOUNCEMENT_CHANNEL_NAME = 'leetcode-anouncement' #must be in channel format, lowercase dash for spaces
-ANOUNCEMENT_MINUTE = 24
-ANOUNCEMENT_HOUR = 13
+TOKEN = 'MTE1NDkwNjc1MTk0NzI1NTg1OQ.G30zzv.u9IvdohrXYF5fKl9QnLLRto84xvneFTvuxqIdw'  # put your token here
+COMMAND_PREFIX = '$'  # Command prefix which can be changed if conflicting with other applications
+CATEGORY_NAME = 'Leetcode Daily'  # Category name that the bot will create and look for
+ANOUNCEMENT_CHANNEL_NAME = 'leetcode-anouncement'  # must be in channel format, lowercase dash for spaces
+ANOUNCEMENT_MINUTE = 5  # The minute of the hour the daily leetcode will be announced
+ANOUNCEMENT_HOUR = 0 # The hour of the day the leetcode with be announced
